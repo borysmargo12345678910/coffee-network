89bf5fafa4f84d37687d1f05255b101ac95ed593
borysmargo12345678910 <borysmargo@gmail.com>
2026-05-16 21:59:17 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,9 @@
 from app import create_app, db
-from flask_migrate import upgrade
 
 app = create_app()
 
 with app.app_context():
-    upgrade()
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
